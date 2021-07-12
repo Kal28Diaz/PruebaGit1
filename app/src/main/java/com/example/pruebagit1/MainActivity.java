8aff31791ad786488f7cc1d3d15208784e7e3e96
Kal28Diaz <80556790+Kal28Diaz@users.noreply.github.com>
2021-07-11 19:33:31 -0500

    Agregue Wiring Up y OnClick para el boton 1 de la Main Activity (con un toast).

--- app/src/main/java/com/example/pruebagit1/MainActivity.java
+++ app/src/main/java/com/example/pruebagit1/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.pruebagit1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button boton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Wiring Up
+        boton1 = findViewById(R.id.button);
+
+        //onClicks
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Boton 1 presionado", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
